d4ae3b24770dbf90cbc5040c47927b90b802dc67
Oleg M <moleg_9432@rambler.ru>
2022-11-25 01:41:43 +0300

    Home work 2 - IP sort

--- gtest_version.cpp
+++ gtest_version.cpp
@@ -8,3 +8,25 @@ TEST(SomeGtests, TestVersionA){
     // Assert
     ASSERT_TRUE(version() > 0);
 }
+
+TEST(SomeGtests, TestIpList) {
+    std::vector<std::vector<std::string> > ip_pool; // Список, который сортируем.
+    std::vector<std::vector<std::string> > ip_pool_control; // Контрольный список.
+
+    ASSERT_TRUE(get_ip_list_from_file(ip_pool, std::string{ "ip_filter.tsv" }));
+    ASSERT_TRUE(get_ip_list_from_file(ip_pool_control, std::string{ "control_ip_list.txt" }));
+
+    reverse_sort(ip_pool); // Сортировка.
+
+    // Сравнение отсортированного списка с контрольным.
+    ASSERT_TRUE(ip_pool.size() == ip_pool_control.size());
+
+    auto ip_iter = ip_pool.begin();
+    auto ip_iter_control = ip_pool_control.begin();
+
+    while (ip_iter != ip_pool.end()) {
+        ASSERT_TRUE(*ip_iter == *ip_iter_control);
+        ++ip_iter;
+        ++ip_iter_control;
+    }
+}
--- lib.h
+++ lib.h
@@ -1,3 +1,35 @@
 #pragma once
 
+#include <string>
+#include <vector>
+
+// Версия программы.
 int version();
+
+// Чтение списка ip адресов из указанного файла.
+bool get_ip_list_from_file(std::vector<std::vector<std::string> >& vvs, const std::string& filename);
+
+// Запись списка ip адресов в файл.
+bool write_ip_list_to_file(std::vector<std::vector<std::string> >& vvs_ip, const std::string& filename);
+
+// Разделение строки  на вектор строк через указанный символ.
+std::vector<std::string> split(const std::string& str, char d);
+
+// Печать вектора-строк, представляющий ip адрес.
+void print_ip_address(const std::vector<std::string>& ip_addr);
+
+// Вывод на экран вектора с ip адресами - векторами строк .
+void print_ip_vector(const std::vector<std::vector<std::string> >& vvs );
+
+// Спрашиваем пользователя, выводить ли в печать.
+bool ask_to_print(const std::string& question);
+
+// Сортировка в обратном порядке.
+void reverse_sort(std::vector<std::vector<std::string> >& vvs_ip);
+
+// Отсортированный вывод в консоль.
+void filter_output(const std::vector<std::vector<std::string> >& vvs_ip, const std::vector<int>& vi_numbers);
+
+// Отсортированный вывод в консоль по значению любой октеты.
+void filter_output_by_any_octet(const std::vector<std::vector<std::string> >& vvs_ip, int value);
+
